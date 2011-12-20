dd742e2d8a9fa3597f47225e73e63388dbb0d4f7
TK Kocheran <rfkrocktk@gmail.com>
2011-12-19 19:50:43 -0800

    Things are basically working now.

--- src/main/java/org/tkassembled/maven/jasperreports/JasperReportsApplication.java
+++ src/main/java/org/tkassembled/maven/jasperreports/JasperReportsApplication.java
@@ -1,12 +1,16 @@
 package org.tkassembled.maven.jasperreports;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.net.URI;
-import java.net.URL;
+import java.io.InputStream;
+import java.util.HashMap;
 
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,6 +27,33 @@ public class JasperReportsApplication {
 	 * 		<X> Export it.
 	 */
 	public static void main(String[] args) throws Exception {
+		logger.debug("Starting up...");
+//		first, we open the XML template
+		logger.info("Opening the JRXML template file.");
+		InputStream template = JasperReportsApplication.class
+			.getResourceAsStream("/sampleReport.jrxml");
 		
+//		now, fill out the report
+		logger.info("Compiling report and filling it out.");
+		JasperReport report = JasperCompileManager.compileReport(template);
+		JasperPrint print = JasperFillManager.fillReport(report, new HashMap<String, String>());
+		
+//		finally, export it to PDF
+		logger.info("Exporting the filled out report to a file.");
+		File pdfOutputFile = File.createTempFile("jasper", ".pdf");
+		pdfOutputFile.deleteOnExit();
+		JasperExportManager.exportReportToPdfStream(print, new FileOutputStream(pdfOutputFile));
+		
+//		now sleep indefinitely to allow for preview but to wipe the files on JVM exit
+		logger.info("Done with export. Find the file at \"{}\" then end this process when done.",
+				pdfOutputFile.getAbsolutePath());
+		
+		while (true) {
+			try {
+				Thread.sleep(Long.MAX_VALUE);
+			} catch (InterruptedException e) {
+				logger.info("Exiting now.");
+			}
+		}
 	}
 }
